791b7e0c9c34ad093fd8ad720789cef4f754c4e4
Your Name <lamisazamzam@gmail.com>
2021-05-28 21:07:07 +0600

    Created the Login Page

---
+++ src/Components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBw6U80A_c455yYBEdctDnGHAU6yDVBQ7E",
+    authDomain: "full-stack-sport-grocery.firebaseapp.com",
+    projectId: "full-stack-sport-grocery",
+    storageBucket: "full-stack-sport-grocery.appspot.com",
+    messagingSenderId: "310660940705",
+    appId: "1:310660940705:web:117b73e2803e671d2901e0",
+};
+
+export default firebaseConfig;
