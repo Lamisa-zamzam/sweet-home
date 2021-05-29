47d95b0e3fad865bd1a11042b72af4a50cfa3c5f
Md Shahadat Hossain Manik <33002552+shmanik14@users.noreply.github.com>
2021-05-29 15:34:51 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './components/Home/Home/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import ApartmentDetails from './components/Home/ApartmentDetails/ApartmentDetails';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <div className="main">
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/apartment/:id">
+              <ApartmentDetails />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
   );
 }
 
