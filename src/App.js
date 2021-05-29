f77d8eb70455864f65f889bc34b72c0c4aeeb329
Your Name <lamisazamzam@gmail.com>
2021-05-29 13:13:29 +0600

    Added Login Page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import './App.css';
+import { createContext, useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import PrivateRoute from "./components/Login/PrivateRoute/PrivateRoute";
+import Login from "./components/Login/Login/Login";
+import "./App.css";
+
+export const UserContext = createContext();
 
 function App() {
-  return (
-    <div>
-    </div>
-  );
+    const [user, setUser] = useState({
+        name: "",
+        email: "",
+        photoURL: "",
+        isNewUser: true,
+        isLoggedIn: false,
+        error: "",
+    });
+
+    return (
+        <UserContext.Provider value={[user, setUser]}>
+            <Router>
+                <Switch>
+                    <Route path="/login">
+                        <Login />
+                    </Route>
+                </Switch>
+            </Router>
+        </UserContext.Provider>
+    );
 }
 
 export default App;
