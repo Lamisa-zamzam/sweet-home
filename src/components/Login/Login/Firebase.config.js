4c30e71e2c0701016432700ca00e0fa6cbfa0e26
Your Name <lamisazamzam@gmail.com>
2021-05-30 10:52:33 +0600

    Delete Login

--- src/components/Login/Login/Firebase.config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyB0afLqZTnZkaw-1BsLPbsOimHlevDEQNo",
-    authDomain: "sweet-home-910.firebaseapp.com",
-    projectId: "sweet-home-910",
-    storageBucket: "sweet-home-910.appspot.com",
-    messagingSenderId: "205102338819",
-    appId: "1:205102338819:web:a34fabc5d875568b460f36"
-};
